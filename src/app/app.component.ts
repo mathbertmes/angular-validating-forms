97f8d8d8fe495f81632095fb42159d146bc1c949
Matheus Bertemes <mathbertemes@gmail.com>
2024-08-08 21:01:48 -0700

    feat: main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BuiltInValidatorComponent } from './components/reactive/built-in-validator/built-in-validator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BuiltInValidatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
